aa3fe13e6468f5fd74bb7e0f7eacbf3aa6a341bc
Stanley Su <stanleysu00@gmail.com>
2021-10-02 13:50:17 +1000

    Add enum to describe moves

--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,8 @@ mod facelet_model;
 pub use facelet_model::*;
 mod vec3;
 pub use vec3::*;
+mod geometry_model;
+pub use geometry_model::*;
 
 #[derive(Copy, Clone, Debug, PartialEq, Eq)]
 pub enum Face {
@@ -11,4 +13,27 @@ pub enum Face {
     R,
     B,
     D,
+}
+
+#[derive(Copy, Clone, Debug, PartialEq, Eq)]
+pub enum Move {
+    // typical moves
+    U,
+    Uw,
+    L,
+    Lw,
+    F,
+    Fw,
+    R,
+    Rw,
+    B,
+    D,
+    // slice moves
+    E,
+    M,
+    S,
+    // rotations
+    X,
+    Y,
+    Z,
 }
